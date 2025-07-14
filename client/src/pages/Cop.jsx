c42c24832be47503bbb18b0079842ba750cb18a0
shab1b1 <240359r@mymail.nyp.edu.sg>
2025-07-14 15:47:54 +0800

    Update Cop.jsx

--- client/src/pages/Cop.jsx
+++ client/src/pages/Cop.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Grid, Card, CardContent } from '@mui/material';
 import http from '../http';
 
 function Cop() {
@@ -10,14 +10,41 @@ function Cop() {
             console.log(res.data);
             setCopList(res.data);
         });
-    }, []); 
+    }, []);
 
     return (
         <Box>
             <Typography variant="h5" sx={{ my: 2 }}>
                 Code of Practices
             </Typography>
-            {/* Add your content here */}
+
+            <Grid container spacing={2}>
+                {
+                    copList.map((cop, i) => {
+                        return (
+                            <Grid item xs={12} md={6} lg={4} key={tutorial.id}>
+                                <Card>
+                                    <CardContent>
+                                        <Typography variant="h6" sx={{ mb: 1 }}>
+                                            {cop.title}
+                                        </Typography>
+                                        <Typography sx={{ whiteSpace: 'pre-wrap' }}>
+                                            {cop.description}
+                                        </Typography>
+                                        <Typography variant="body2">
+                                            {cop.authority}
+                                        </Typography>
+                                        <Typography variant="body2">
+                                            {cop.url}
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        );
+                    })
+                }
+            </Grid>
+
         </Box>
     );
 }
